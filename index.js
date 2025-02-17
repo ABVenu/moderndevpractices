72811f2dea6893f753552bad5d8239817cf5c78a
ABVenu <avenugopalb@gmail.com>
2025-02-17 16:03:27 +0530

    Linting Check

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 const express = require("express");
 const connectToDB = require("./config/mongo.config");
 const app = express();
-
-if(process.env.NODE_ENV=="testing"){
-    require("dotenv").config({path:'.env.testing'})
-}else if(process.env.NODE_ENV=="production"){
-    require("dotenv").config({path:'.env.production'})
-}else{
-    require("dotenv").config()
+console.log("Hi");
+console.log(Hi);
+let a = 10;
+if (process.env.NODE_ENV == "testing") {
+  require("dotenv").config({ path: ".env.testing" });
+} else if (process.env.NODE_ENV == "production") {
+  require("dotenv").config({ path: ".env.production" });
+} else {
+  require("dotenv").config();
 }
 
-const PORT = process.env.PORT || 8080
-app.use(express.json())
+const PORT = process.env.PORT || 8080;
+app.use(express.json());
 
-app.get("/", (req,res)=>{
-    try{
-        res.status(200).json({msg:"This is test route"})
-    }catch(err){
-        res.status(500).json({msg:"Something went wrong in the test route"})
-    }
-})
-app.listen(PORT, ()=>{
-    connectToDB()
-    console.log("Server Started on the Port", PORT)
-})
+app.get("/", (req, res) => {
+  try {
+    res.status(200).json({ msg: "This is test route" });
+  } catch (err) {
+    res.status(500).json({ msg: "Something went wrong in the test route" });
+  }
+});
+app.listen(PORT, () => {
+  connectToDB();
+  console.log("Server Started on the Port", PORT);
+});
