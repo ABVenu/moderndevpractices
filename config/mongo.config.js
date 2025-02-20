481bb6407588da8d8714740289f38d7b3912fedc
ABVenu <avenugopalb@gmail.com>
2025-02-20 16:51:36 +0530

    some changes

--- config/mongo.config.js
+++ config/mongo.config.js
@@ -3,13 +3,13 @@ console.log("Hi")
 console.log(Hi)
 let a = 10
 
-                         const               connectToDB = async ()=>{
+                         const                 connectToDB = async ()=>{
     // console.log( process.env.MONGO_URL)
-    try{
-                      await mongoose.connect(process.env.MONGO_URL)
+    try{ 
+                        await mongoose.connect(process.env.MONGO_URL)
                       console.log("connected to DB with link", process.env.MONGO_URL)
     }catch(err){
-                                 console.log("Failed to connect to db")
+                                   console.log("Failed to connect to db")
     }
 }
 
